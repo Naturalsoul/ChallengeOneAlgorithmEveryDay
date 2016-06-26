759410b402f95f089c60009231e702946f01aa75
Naturalsoul <raul.d.carrasco@outlook.com>
2016-06-26 14:23:56 -0400

    Main class created.

---
+++ src/JavaAlgorithms/Main.java
@@ -0,0 +1,19 @@
+package JavaAlgorithms;
+
+/**
+ * Hey!! This is a list of algorithms. Originally, these algorithms are from FreeCodeCamp.com where you
+ * need to code them in Javascript. I will convert them to Java.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    private static void menu () {
+        System.out.println("--------------------");
+        System.out.println("Hi!! :D. Choose an algorithm!");
+        System.out.println("1) Chunkey Monkey.");
+        System.out.println("--------------------");
+    }
+}
