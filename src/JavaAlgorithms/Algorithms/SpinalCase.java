e0e5f4bfba94888b064f9b428b0617b6552193dc
Naturalsoul <raul.d.carrasco@outlook.com>
2016-07-03 15:34:51 -0400

    Spinal Case Class commentary changed.

--- src/JavaAlgorithms/Algorithms/SpinalCase.java
+++ src/JavaAlgorithms/Algorithms/SpinalCase.java
@@ -1,7 +1,11 @@
 package JavaAlgorithms.Algorithms;
 
+import java.util.Scanner;
+
 /**
  * Converts a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+ * This is a very simple algorithm. Today, I don't feel sooo good and i decided to write an
+ * easy algorithm. I hope I'll feel better soon.
  */
 
 public class SpinalCase {
@@ -14,5 +18,13 @@ public class SpinalCase {
         return sentence;
     }
 
+    public void showAlg (Scanner reader) {
+        System.out.println("\nConverts a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.");
+        System.out.println("Enter a sentence:");
+        reader.nextLine();
 
+        String sentence = reader.nextLine();
+
+        System.out.println("\nResult: " + this.getSpinalCaseString(sentence));
+    }
 }
