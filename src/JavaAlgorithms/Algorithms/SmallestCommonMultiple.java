b789872498f050c6d617fc90ae5ba14accea0fe6
Naturalsoul <raul.d.carrasco@outlook.com>
2016-07-07 14:46:02 -0400

    Function fixed and exceptions captured.

--- src/JavaAlgorithms/Algorithms/SmallestCommonMultiple.java
+++ src/JavaAlgorithms/Algorithms/SmallestCommonMultiple.java
@@ -10,22 +10,25 @@ import java.util.Scanner;
  * The range will be an array of two numbers that will not necessarily be in numerical order.
  *
  * e.g. for 1 and 3 - find the smallest common multiple of both 1 and 3 that is evenly divisible by all numbers between 1 and 3.
+ *
+ * I think this algorithm could be optimized. I hope you can help me :)
  */
 
 public class SmallestCommonMultiple {
-    private int smallest;
-    private int larger;
-    private ArrayList<Integer> range;
-    private ArrayList<Integer> smallestTable;
-    private ArrayList<Integer> largerTable;
-    private int scm;
+
 
     public SmallestCommonMultiple () {}
 
     private int getSmallestCommonMultiple (Integer[] numbers) {
+        int smallest;
+        int larger;
+        ArrayList<Integer> range = new ArrayList<>();
+        ArrayList<Integer> smallestTable = new ArrayList<>();
+        ArrayList<Integer> largerTable = new ArrayList<>();
+        int scm = 0;
         boolean aPossibleSCM = false;
 
-        if (numbers.length < 2) return 0;
+        if (numbers.length < 2 || numbers[0] < 1 && numbers[1] < 1) return 0;
 
         smallest = Math.min(numbers[0], numbers[1]);
         larger = Math.max(numbers[0], numbers[1]);
@@ -38,10 +41,10 @@ public class SmallestCommonMultiple {
             largerTable.add(larger * i);
         }
 
-        for (int i = 0; i < smallestTable.size(); i++) {
-            if (smallestTable.indexOf(largerTable.get(i)) != -1) {
+        for (Integer aSmallestTableNumber : smallestTable) {
+            if (largerTable.indexOf(aSmallestTableNumber) != -1) {
                 for (Integer aRangeNumber : range) {
-                    if (smallestTable.get(i) % aRangeNumber == 0)
+                    if (aSmallestTableNumber % aRangeNumber == 0)
                         aPossibleSCM = true;
                     else {
                         aPossibleSCM = false;
@@ -50,7 +53,8 @@ public class SmallestCommonMultiple {
                 }
 
                 if (aPossibleSCM) {
-                    scm = smallestTable.get(i);
+                    scm = aSmallestTableNumber;
+                    break;
                 }
 
             }
@@ -68,12 +72,20 @@ public class SmallestCommonMultiple {
         System.out.print("\nEnter the first number: ");
         reader.nextLine();
 
-        arr[0] = Integer.valueOf(reader.findInLine("[0-9]+"));
+        try {
+            arr[0] = Integer.valueOf(reader.findInLine("[0-9]+"));
+        } catch (Exception ex) {
+            arr[0] = 0;
+        }
 
         System.out.print("Enter the second number: ");
         reader.nextLine();
 
-        arr[1] = Integer.valueOf(reader.findInLine("[0-9]+"));
+        try {
+            arr[1] = Integer.valueOf(reader.findInLine("[0-9]+"));
+        } catch (Exception ex) {
+            arr[1] = 0;
+        }
 
         System.out.println("\nResult: " + this.getSmallestCommonMultiple(arr));
     }
