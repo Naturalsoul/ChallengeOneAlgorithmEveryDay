87da243a477bdc2d562e463425c6f232e6d842de
Naturalsoul <raul.d.carrasco@outlook.com>
2016-07-10 17:39:56 -0400

    HarmonicMean algorithm added to Main Class.

--- src/JavaAlgorithms/Main.java
+++ src/JavaAlgorithms/Main.java
@@ -165,6 +165,11 @@ public class Main {
                     npv.showAlg(reader);
                     break;
 
+                case 15:
+                    HarmonicMean hm = new HarmonicMean();
+                    hm.showAlg(reader);
+                    break;
+
                 case 19:
                     System.out.println("\nSee ya!!");
                     System.exit(0);
@@ -190,6 +195,7 @@ public class Main {
         System.out.println("12) Smallest Common Multiple.");
         System.out.println("13) Friendly Numbers.");
         System.out.println("14) Net Present Values.");
+        System.out.println("15) Harmonic Mean.");
         System.out.println("19) Thanks for the algorithms. Good Bye!!");
         System.out.println("--------------------");
         System.out.print("Choose an option: ");
