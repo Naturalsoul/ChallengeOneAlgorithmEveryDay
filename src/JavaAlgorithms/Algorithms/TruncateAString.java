1f917bcf43bbfb3ce12d818cdef519d881604674
Naturalsoul <raul.d.carrasco@outlook.com>
2016-07-13 17:32:15 -0400

    TruncateAString's showAlg function added.

--- src/JavaAlgorithms/Algorithms/TruncateAString.java
+++ src/JavaAlgorithms/Algorithms/TruncateAString.java
@@ -1,5 +1,7 @@
 package JavaAlgorithms.Algorithms;
 
+import java.util.Scanner;
+
 /**
  * Truncates a string (first argument) if it is longer than the given maximum
  * string length (second argument). Returns the truncated string with a ... ending.
@@ -20,5 +22,30 @@ public class TruncateAString {
         str = str.concat("...");
 
         return str;
+    }
+
+    public void showAlg (Scanner reader) {
+        String str;
+        int num;
+
+        System.out.println("\nTruncates a string (first argument) if it is longer than the");
+        System.out.println("given maximum string length (second argument). Return the truncated string with a ... ending.");
+
+        System.out.print("\nEnter a sentence: ");
+        reader.nextLine();
+
+        str = reader.nextLine();
+
+        System.out.print("Enter the number of character you want to keep: ");
+        reader.nextLine();
+
+        try {
+            num = Integer.valueOf(reader.findInLine("[0-9]+"));
+        } catch (Exception ex) {
+            num = 0;
+        }
+
+        System.out.println("\nResult:");
+        System.out.println(this.getAStringTruncated(str, num));
     }
 }
