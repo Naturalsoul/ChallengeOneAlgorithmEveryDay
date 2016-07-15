e3c6d072be5029cfa6601aa4f6013efe6a231542
Naturalsoul <raul.d.carrasco@outlook.com>
2016-07-15 00:39:27 -0400

    FactorializeANumber's showAlg function added.

--- src/JavaAlgorithms/Algorithms/FactorializeANumber.java
+++ src/JavaAlgorithms/Algorithms/FactorializeANumber.java
@@ -1,5 +1,7 @@
 package JavaAlgorithms.Algorithms;
 
+import java.util.Scanner;
+
 /**
  * Returns the factorial of the provided integer.
  */
@@ -10,5 +12,21 @@ public class FactorializeANumber {
     private int getFactorial (int n) {
         if (n == 0) return 1;
         return getFactorial(n - 1);
+    }
+
+    public void showAlg (Scanner reader) {
+        int n;
+
+        System.out.println("\nReturns the factorial of the provided integer.");
+        System.out.print("\nEnter a number: ");
+        reader.nextLine();
+
+        try {
+            n = Integer.valueOf(reader.findInLine("[0-9]+"));
+        } catch (Exception ex) {
+            n = 0;
+        }
+
+        System.out.println("\nResult: " + this.getFactorial(n));
     }
 }
