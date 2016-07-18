1ab074701efc3b1307d5926bea905f7338c69789
Naturalsoul <raul.d.carrasco@outlook.com>
2016-07-17 20:37:48 -0400

    RepeatAStringRepeatAString's showAlg function added.

--- src/JavaAlgorithms/Algorithms/RepeatAStringRepeatAString.java
+++ src/JavaAlgorithms/Algorithms/RepeatAStringRepeatAString.java
@@ -1,6 +1,7 @@
 package JavaAlgorithms.Algorithms;
 
 import java.util.Collections;
+import java.util.Scanner;
 
 /**
  * Repeats a given string (first argument) num times (second argument).
@@ -16,5 +17,27 @@ public class RepeatAStringRepeatAString {
         }
 
         return new String(new char[howManyTimes]).replace("\0", str);
+    }
+
+    public void showAlg (Scanner reader) {
+        System.out.println("\nRepeats a given string (first argument) num times (second argument).");
+        System.out.println("Returns an empty string if num is not a positive number.");
+
+        System.out.print("\nEnter a word: ");
+        reader.nextLine();
+
+        String str = reader.nextLine();
+
+        System.out.print("How many times? : ");
+        int times;
+
+        try {
+            times = Integer.valueOf(reader.findInLine("[0-9]+"));
+        } catch (Exception ex) {
+            times = 0;
+        }
+
+        System.out.println("\nResult:");
+        System.out.println(this.repeatThis(str, times));
     }
 }
