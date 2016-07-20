9c118353daeba238cb9460c66f05bd1dd1cdb861
Naturalsoul <raul.d.carrasco@outlook.com>
2016-07-20 09:36:30 -0400

    Palindromes' showAlg function added.

--- src/JavaAlgorithms/Algorithms/Palindromes.java
+++ src/JavaAlgorithms/Algorithms/Palindromes.java
@@ -2,6 +2,7 @@ package JavaAlgorithms.Algorithms;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Scanner;
 
 /**
  * Returns true if the given string is a palindrome. Otherwise, returns false.
@@ -27,5 +28,20 @@ public class Palindromes {
         reverseStr = String.join("", (CharSequence[]) arr);
 
         return str.equals(reverseStr);
+    }
+
+    public void showAlg (Scanner reader) {
+        String str;
+
+        System.out.println("\nReturns true if the given string is a palindrome. Otherwise, returns false.");
+        System.out.println("A palindrome is a word or sentence that's spelled the same way both forward");
+        System.out.println("and backward, ignoring punctuation, case, and spacing.");
+
+        System.out.println("\nEnter a word or a sentence: ");
+        reader.nextLine();
+
+        str = reader.nextLine();
+
+        System.out.println("\nResult: " + this.checkPalindrome(str));
     }
 }
