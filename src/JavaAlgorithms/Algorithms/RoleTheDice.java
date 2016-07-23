daab355ca683892b584be02cd11082138b1f7197
Naturalsoul <raul.d.carrasco@outlook.com>
2016-07-23 01:46:21 -0400

    RoleTheDice's showAlg function added.

--- src/JavaAlgorithms/Algorithms/RoleTheDice.java
+++ src/JavaAlgorithms/Algorithms/RoleTheDice.java
@@ -1,5 +1,8 @@
 package JavaAlgorithms.Algorithms;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Dice notation (also known as dice algebra, common dice notation, RPG dice notation, and several other titles)
  * is a system to represent different combinations of dice in role-playing games using simple algebra-like notation such as 2d6+12.
@@ -50,5 +53,57 @@ public class RoleTheDice {
         }
 
         return (int) (Math.random() * times) + 1;
+    }
+
+    public void showAlg (Scanner reader) {
+        int dice, times, sum = 0;
+        ArrayList<String> nums = new ArrayList<>();
+        String allTheNumbers;
+
+        System.out.println("\nDice notation (also known as dice algebra, common dice notation, RPG dice notation, and several other titles)");
+        System.out.println("is a system to represent different combinations of dice in role-playing games using simple algebra-like notation such as 2d6+12.");
+        System.out.println("Here, you can roll a D4, D6, D8, D10, D12, D20, D30, D100.");
+
+        System.out.println("\nDices ----------");
+        System.out.println("1) D4.");
+        System.out.println("2) D6.");
+        System.out.println("3) D8.");
+        System.out.println("4) D10.");
+        System.out.println("5) D12.");
+        System.out.println("6) D20.");
+        System.out.println("7) D30.");
+        System.out.println("8) D100.");
+        System.out.println("------------------");
+
+        System.out.print("Select a dice: ");
+        reader.nextLine();
+
+        try {
+            dice = Integer.valueOf(reader.findInLine("[1-8]"));
+        } catch (Exception ex) {
+            dice = 0;
+        }
+
+        System.out.print("How many times?: ");
+        reader.nextLine();
+
+        try {
+            times = Integer.valueOf(reader.findInLine("[1-9]"));
+        } catch (Exception ex) {
+            times = 1;
+        }
+
+        System.out.println("\nResult:");
+
+        for (int i = 0; i < times; i++) {
+            int n = this.rollIt(dice);
+            sum += n;
+
+            nums.add(String.valueOf(n));
+        }
+
+        allTheNumbers = String.join(" + ", nums);
+
+        System.out.println(allTheNumbers + " = " + sum);
     }
 }
