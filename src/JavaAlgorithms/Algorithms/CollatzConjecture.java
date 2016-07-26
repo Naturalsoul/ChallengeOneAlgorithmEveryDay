17a6f335728f602184fe2baf35a765643df9c9d9
Naturalsoul <raul.d.carrasco@outlook.com>
2016-07-26 01:10:37 -0400

    CollatzConjecture's showAlg function added.

--- src/JavaAlgorithms/Algorithms/CollatzConjecture.java
+++ src/JavaAlgorithms/Algorithms/CollatzConjecture.java
@@ -1,12 +1,13 @@
 package JavaAlgorithms.Algorithms;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * The sequence of Collatz of an integer is constructed in the following way:
  *
  *  - If the number is even, it is divided by two;
- *  - If the number is even, it is divided by two;
+ *  - If is odd, it multiplies three and adds one;
  *  - the succession ends upon arrival at one.
  *
  * The Collatz conjecture says that, starting from any number, the sequence will always reach 1.
@@ -20,7 +21,7 @@ public class CollatzConjecture {
     public CollatzConjecture () {}
 
     private ArrayList<Integer> getCollatz (int number) {
-        if ((2 ^ 58) >= number) {
+        if ((2 ^ 58) >= number || number == 0) {
             return new ArrayList<>(0);
         }
 
@@ -34,5 +35,41 @@ public class CollatzConjecture {
         }
 
         return arr;
+    }
+
+    public void showAlg (Scanner reader) {
+        int number;
+        ArrayList<Integer> arr;
+        String result = "";
+
+        System.out.println("\nThe sequence of Collatz of an integer is constructed in the following way:");
+        System.out.println("- If the number is even, it is divided by two;");
+        System.out.println("- If is odd, it multiplies three and adds one;");
+        System.out.println("- the succession ends upon arrival at one.");
+
+        System.out.println("\nThe Collatz conjecture says that, starting from any number, the sequence will always reach 1.");
+        System.out.println("Despite being an affirmation to simple sight is very simple, it has not been able to demonstrate whether it is true or not.");
+
+        System.out.println("\nUsing computers, it has been verified that the succession actually");
+        System.out.println("reaches 1 departing from any natural number less than 2^58.");
+
+        System.out.print("\nEnter a number bellow than 2^58: ");
+        reader.nextLine();
+
+        try {
+            number = Integer.valueOf(reader.findInLine("[0-9]+"));
+        } catch (Exception ex) {
+            number = 0;
+        }
+
+        System.out.println("\nResult:");
+
+        arr = this.getCollatz(number);
+
+        for (Integer n : arr) {
+            result += n + " ";
+        }
+
+        System.out.println(result);
     }
 }
